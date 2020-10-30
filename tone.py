b0ee44e8831ad8943ecc195d46d93332f0ad5825
JontePonte <john_nordstrand@hotmail.com>
2020-10-30 20:18:06 +0100

    added octave -1 and title

--- tone.py
+++ tone.py
@@ -1,9 +1,13 @@
 
 x = 2**(1/12)
-freq = 130.81278265029934  # c0
+freq = 130.81278265029934 / 2  # c-1
 notes = ['C ','C#','D ','Eb','E ','F ','F#','G ','Ab','A ','Bb','B ']
 
-for octave in range(0,4):
+
+print()
+print('Frequencies of notes:')
+
+for octave in range(-1,4):
     print(f'octave {octave}:')
     for note in notes:
         print(f'{note}: {round(freq,1)}')
